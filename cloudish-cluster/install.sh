0ca1cbbe8f6867085caf6087e26e18c6e273a012
root <root@kube-master-1.lsd.ufcg.edu.br>
2018-04-26 09:57:59 -0300

    removing go installation from install.sh

--- cloudish-cluster/install.sh
+++ cloudish-cluster/install.sh
@@ -16,18 +16,6 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet=1.9.1-00 kubeadm=1.9.1-00 kubectl=1.9.1-00 kubernetes-cni
+apt-get install kubelet=1.9.1-00 kubeadm=1.9.1-00 kubectl=1.9.1-00 kubernetes-cni
 
-# Install go
-sudo apt-get update
-sudo apt-get -y upgrade
-wget https://dl.google.com/go/go1.9.4.linux-amd64.tar.gz
-sudo tar -xvf go1.9.4.linux-amd64.tar.gz
-sudo mv go /usr/local
 
-# edit .profile
-echo "export GOROOT=/usr/local/go" >> ~/.profile
-echo "export GOPATH=/usr/share/go/bin" >> ~/.profile
-echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.profile
-source ~/.profile
-echo $PATH
