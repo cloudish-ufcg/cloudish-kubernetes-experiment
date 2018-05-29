3097636350520b19f7e73d5d402641dd0e5bab7d
fabiomorais <fabio.jorge@gmail.com>
2018-05-29 13:37:52 -0300

    Fix import

--- broker/broker.go
+++ broker/broker.go
@@ -13,7 +13,7 @@ import (
 	"sync"
 	"time"
 
-	appsv1beta2 "github.com/prometheus/client_golang/api"
+	appsv1beta2 "k8s.io/api/apps/v1beta2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
